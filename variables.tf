05a2249d2fc87e36a722329606174c36cb48f528
Andrii Doronin <andrii_doronin@epam.com>
2021-10-26 15:16:48 +0300

    pre release

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "instance-type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "region-master" {
+  type    = string
+  default = "eu-central-1"
+}
+variable "profile" {
+  type    = string
+  default = "default"
+}
