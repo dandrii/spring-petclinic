e78e628741323316a5be02fae6761b99d9168b27
Andrii Doronin <andrii_doronin@epam.com>
2021-10-28 17:02:48 +0300

    test

--- Jenkinsfile
+++ Jenkinsfile
@@ -7,5 +7,13 @@ pipeline {
                 echo "Hello ${params.image}"
             }
         }
+        stage('DEPLOY QA ENV') {
+            steps {
+                sh 'terraform init'
+                sh 'terraform destroy -auto-approve'
+                sh 'terraform apply --var="dockerimage=${params.image}" -auto-approve'
+            }
+        }
+
     }
 }
---
+++ instances.tf
@@ -0,0 +1,32 @@
+#Get Linux AMI ID using SSM Parameter endpoint in eu-central-1
+data "aws_ssm_parameter" "PetclinicAmi" {
+  provider = aws.region-master
+  name     = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+#Create key-pair for logging into EC2 in eu-central-1
+resource "aws_key_pair" "master-key" {
+  provider   = aws.region-master
+  key_name   = "petclinic"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+#Create and bootstrap EC2 in eu-central-1
+resource "aws_instance" "petclinic" {
+  provider                    = aws.region-master
+  ami                         = data.aws_ssm_parameter.PetclinicAmi.value
+  instance_type               = var.instance-type
+  key_name                    = aws_key_pair.master-key.key_name
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "petclinic_tf_qa"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region-master} --instance-ids ${self.id} \
+&& ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name} image=${var.dockerimage}' ansible-petclinic-qa-deploy.yml
+EOF
+  }
+}
